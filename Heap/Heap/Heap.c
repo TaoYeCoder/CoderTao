fd4f41efecf1c069b56cf701b1e1750d98388937
yeshengtao <2304795947@qq.com>
2023-11-06 02:09:57 +0800

    "二叉数2"

--- Heap/Heap/Heap.c
+++ Heap/Heap/Heap.c
@@ -14,16 +14,25 @@ void HeapDestroy(HP* php)
 }
 
 
-void AdjustDwon(HPDataType*a,int child)
+void Swap(HPDataType* p1, HPDataType* p2)
+{  
+	HPDataType tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
+}
+
+
+void AdjustUp(HPDataType*a,int child)
 {
 	int parent = (child - 1) / 2;
 	while (0 < child)
 	{
 		if (a[child] < a[parent])
 		{
-			HPDataType* tem = a[child];
-			a[child] = a[parent];
-			a[parent] = tem;
+			//1.先交换
+			Swap(&a[child], &a[parent]);
+
+			//2.向上调
 			child = parent;
 			parent = (child - 1) / 2;
 		}
@@ -53,5 +62,52 @@ void HeapPush(HP* php, HPDataType x)
 	php->size++;
 
 	//向上调整，要把结构体的数组和数组的最后一位元素传过去当孩子
-	AdjustDwon(php->a, php->size - 1);
+	AdjustUp(php->a, php->size - 1);
+}
+
+AdjustDown(int* a, int n, int parent)
+{
+	int chile = parent * 2 + 1;
+	while (chile < n)
+	{
+		if (a[chile + 1] < a[chile])
+		{
+			++chile;
+		}
+
+		if (a[parent] > a[chile])
+		{
+			Swap(&a[parent], &a[chile]);
+			parent = chile;
+			chile = parent * 2 + 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+void HeapPop(HP* php)
+{
+	assert(php);
+	assert(!HeapEmpty(php));
+	Swap(&php->a[0], &php->a[php->size - 1]);
+	--php->size;
+	AdjustDown(php->a, php->size, 0);
+}
+
+HPDataType HeapTop(HP* php)
+{
+
+}
+
+bool HeapEmpty(HP* php)
+{
+
+}
+
+int HeapSize(HP* php)
+{
+
 }
--- Heap/Heap/Heap.h
+++ Heap/Heap/Heap.h
@@ -11,7 +11,10 @@ typedef struct Heap
 	int capacity;
 }HP;
 
-
 void HeapInit(HP* php);
 void HeapDestroy(HP* php);
 void HeapPush(HP* php, HPDataType x);
+void HeapPop(HP* php);
+HPDataType HeapTop(HP* php);
+bool HeapEmpty(HP* php);
+int HeapSize(HP*  php);
